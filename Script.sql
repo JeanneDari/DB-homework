ac42d1d35eca08e1a45214f2ed71dff83dd7e324
JeanneDari <zhanna.daribayeva@gmail.com>
2022-03-29 16:54:36 +0600

    initial commit

---
+++ Script.sql
@@ -0,0 +1,9 @@
+
+create table if not exists artist
+(id serial primary key, name varchar(40) not null);
+create table if not exists album
+(id serial primary key, name varchar(40) not null, release_date date, artist_id integer references artist(id));
+create table if not exists songs
+(id serial primary key, track varchar(40) not null, duration integer, album_id integer album(id));
+create table if not exists genres
+(id serial primary key, name varchar(40) not null, artist_id integer references artist(id));
