983a1d9392b8178c19af84cd25013e55b657035b
JeanneDari <zhanna.daribayeva@gmail.com>
2022-04-03 08:23:21 +0600

    изменена связь таблиц artist и genres

--- Script.sql
+++ Script.sql
@@ -1,9 +1,11 @@
 
-create table if not exists artist
+/*create table if not exists artist
 (id serial primary key, name varchar(40) not null);
 create table if not exists album
 (id serial primary key, name varchar(40) not null, release_date date, artist_id integer references artist(id));
 create table if not exists songs
 (id serial primary key, track varchar(40) not null, duration integer, album_id integer album(id));
 create table if not exists genres
-(id serial primary key, name varchar(40) not null, artist_id integer references artist(id));
+(id serial primary key, name varchar(40) not null, artist_id integer references artist(id));*/
+ALTER TABLE genres DROP COLUMN artist_id;
+ALTER TABLE artist ADD genre_id integer references genres(id);
